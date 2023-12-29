86bec7a5a041d315ef7aa5339715ee844f99f888
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2023-12-29 05:50:06 +0530

    Create binarynode.java

---
+++ tree/binarytree/linkedlist/binarynode.java
@@ -0,0 +1,13 @@
+package binarytree_linkedlist;
+
+public class binarynode {
+	String value;
+	binarynode left;
+	binarynode right;
+	binarynode(){
+		
+	}
+	binarynode(String data){
+		this.value = data;
+	}
+}
