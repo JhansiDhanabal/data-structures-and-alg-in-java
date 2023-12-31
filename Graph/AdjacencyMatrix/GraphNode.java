f55973bac0809af6286ccd380f68a8e5074c1c95
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2023-12-31 21:15:32 +0530

    Create GraphNode.java

---
+++ Graph/AdjacencyMatrix/GraphNode.java
@@ -0,0 +1,12 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class GraphNode {
+	String value;
+	int index;
+	GraphNode(String value, int index){
+		this.value = value;
+		this.index = index;
+	}
+}
