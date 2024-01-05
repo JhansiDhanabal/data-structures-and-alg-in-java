a4d6369433b5e78ff4cb2f0209599e33de9475cd
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2024-01-06 00:10:58 +0530

    Create activity.java

---
+++ GreedyAlgorithm/acitivtyselectionproblem/activity.java
@@ -0,0 +1,13 @@
+package greedyalgorithm;
+
+public class activity {
+	String name;
+	int start;
+	int end;
+	activity(String name, int start, int end){
+		this.name = name;
+		this.start = start;
+		this.end = end;
+	}
+
+}
